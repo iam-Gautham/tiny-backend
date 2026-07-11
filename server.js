13af55836593d0c10ca25ec3bb935803dead5fe1
iam-Gautham <gauthampsajith@gmail.com>
2026-07-11 19:49:19 +0530

    bug fix

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-const express=require("express");
-const app=express();
-const PORT=3000;
-app.get("/",(req,res)=>{
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
     res.json({
         message: "Hello, FlyRank!"
     });
 });
-app.get("/",(req,res)=>{
+
+app.get("/about", (req, res) => {
     res.json({
         name: "Gautham P Sajith",
         university: "Model Engineering College",
         role: "Backend Engineering Intern"
     });
 });
-app.listen(PORT,()=>{
+
+app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
